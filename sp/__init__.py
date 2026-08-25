ff843b7bb2ee437ca026a58b34e70b10f5f1eeeb
Joe Hannibal Smith <grnwood@gmail.com>
2026-08-25 12:45:12 -0500

    task candy

--- sp/__init__.py
+++ sp/__init__.py
@@ -1,4 +1,4 @@
-VERSION='v1.2.4a'
+VERSION='v1.2.4b'
 GITHUB_PROJECT='stillpoint'
 GITHUB_OWNER='grnwood'  
 GITHUB_ISSUE_URL="https://github.com/<owner>/<repo>/issues/new?title={title}&body={body}"
